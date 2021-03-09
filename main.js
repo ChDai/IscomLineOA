f1d828d5ac82956605baab7433338dc9a200a309
ChDai <49802856+ChDai@users.noreply.github.com>
2021-03-09 13:44:08 +0800

    no message

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
 function bindAction() {
     console.log("bind");
-    postData("https://2414.api.gosu.bar/customize/postEndBind",null)
+    location.href = "https://line.me/R/oaMessage/@181ofyou/";
 }
-
-function postData(url, data) {
-    return fetch(url, {
-      body: JSON.stringify(data), // must match 'Content-Type' header
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      headers: {
-        'content-type': 'application/json'
-      },
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    })
-    .then(response => response.json()) // 輸出成 json
-  }
